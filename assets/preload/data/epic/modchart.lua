13cd4f2cbfb41ffb665b481738834590a44bd241
glauberb0802 <111463416+glauberb0802@users.noreply.github.com>
2023-09-02 19:32:17 -0300

    Add files via upload

---
+++ assets/preload/data/epic/modchart.lua
@@ -0,0 +1,11 @@
+function onUpdate()
+        noteTweenX('Movement X 0', 0, -240, 0.1)
+        noteTweenX('Movement X 1', 1, -240, 0.1)
+        noteTweenX('Movement X 2', 2, -240, 0.1)
+        noteTweenX('Movement X 3', 3, -240, 0.1)
+
+        noteTweenX('Movement X 4', 4, 200, 0.1)
+        noteTweenX('Movement X 5', 5, 350, 0.1)
+        noteTweenX('Movement X 6', 6, 810, 0.1)
+        noteTweenX('Movement X 7', 7, 990, 0.1)
+end
