f273be5e0284a2ef98fd0ed002369c17b1f72139
glauberb0802 <111463416+glauberb0802@users.noreply.github.com>
2023-09-02 20:11:26 -0300

    Update testCutscene.lua

--- assets/preload/data/power-ups/testCutscene.lua
+++ assets/preload/data/power-ups/testCutscene.lua
@@ -1,7 +1,7 @@
 function onEndSong()
 	if not allowEnd and not seenCutscene then
 		startVideo('power-ups-cutscene');
-		allowEnd = true;
+		allowEnd = false;
 		return Function_Stop;
 	end
 	return Function_Continue;
