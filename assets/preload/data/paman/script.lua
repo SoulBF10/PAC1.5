e9168b0653c771aeedf5d4fd0cd7bd0d1cb2b4e0
glauberb0802 <111463416+glauberb0802@users.noreply.github.com>
2023-09-02 19:33:30 -0300

    Add files via upload

---
+++ assets/preload/data/paman/script.lua
@@ -0,0 +1,95 @@
+
+local xx = 700;
+local yy = 600;
+local xx2 = 900;
+local yy2 = 600;
+local ofs = 35;
+local followchars = true;
+local del = 0;
+local del2 = 0;
+
+function onCreatePost()
+    for i=0, getProperty('unspawnNotes.length'), 1 do
+        setPropertyFromGroup('unspawnNotes', i, 'texture', 'NOTE_assets_Paman')
+    end
+    for i=0, getProperty('opponentStrums.length'), 1 do
+        setPropertyFromGroup('opponentStrums', i, 'texture', 'NOTE_assets_Paman')
+    end
+    for i=0, getProperty('playerStrums.length'), 1 do
+        setPropertyFromGroup('playerStrums', i, 'texture', 'NOTE_assets_Paman')
+    end
+end
+function onUpdate()
+	if del > 0 then
+		del = del - 1
+	end
+	if del2 > 0 then
+		del2 = del2 - 1
+	end
+    if followchars == true then
+        if mustHitSection == false then
+            if getProperty('dad.animation.curAnim.name') == 'singLEFT' then
+                triggerEvent('Camera Follow Pos',xx-ofs,yy)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singRIGHT' then
+                triggerEvent('Camera Follow Pos',xx+ofs,yy)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singUP' then
+                triggerEvent('Camera Follow Pos',xx,yy-ofs)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singDOWN' then
+                triggerEvent('Camera Follow Pos',xx,yy+ofs)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singLEFT-alt' then
+                triggerEvent('Camera Follow Pos',xx-ofs,yy)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singRIGHT-alt' then
+                triggerEvent('Camera Follow Pos',xx+ofs,yy)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singUP-alt' then
+                triggerEvent('Camera Follow Pos',xx,yy-ofs)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singDOWN-alt' then
+                triggerEvent('Camera Follow Pos',xx,yy+ofs)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'idle-alt' then
+                triggerEvent('Camera Follow Pos',xx,yy)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'idle' then
+                triggerEvent('Camera Follow Pos',xx,yy)
+            end
+        else
+
+            if getProperty('boyfriend.animation.curAnim.name') == 'singLEFT' then
+                triggerEvent('Camera Follow Pos',xx2-ofs,yy2)
+            end
+            if getProperty('boyfriend.animation.curAnim.name') == 'singRIGHT' then
+                triggerEvent('Camera Follow Pos',xx2+ofs,yy2)
+            end
+            if getProperty('boyfriend.animation.curAnim.name') == 'singUP' then
+                triggerEvent('Camera Follow Pos',xx2,yy2-ofs)
+            end
+            if getProperty('boyfriend.animation.curAnim.name') == 'singDOWN' then
+                triggerEvent('Camera Follow Pos',xx2,yy2+ofs)
+            end
+	    if getProperty('boyfriend.animation.curAnim.name') == 'idle' then
+                triggerEvent('Camera Follow Pos',xx2,yy2)
+            end
+        end
+    else
+        triggerEvent('Camera Follow Pos','','')
+    end
+    
+end
+
+function onStepHit()
+    if curStep > 272 then
+        doTweenAlpha('hud', 'camHUD', 1, 1, 'linear')
+    end
+end
+
+function onBeatHit()
+    if curBeat > 54 then
+        doTweenAlpha('hud', 'camHUD', 0.6, 1, 'linear')
+    end
+end
